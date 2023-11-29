3921d1539bb71cc8eb93d76402c594f4822fabcc
Ovsyannikov1509 <148484879+Ovsyannikov1509@users.noreply.github.com>
2023-11-29 21:17:47 +0300

    Лабораторная работа №4

---
+++ Лабораторная работа №4/task2.py
@@ -0,0 +1,24 @@
+import csv
+import json
+
+INPUT_FILENAME = "input.csv"
+OUTPUT_FILENAME = "output.json"
+
+
+def task() -> None:
+    with open(INPUT_FILENAME, 'r') as file:
+        reader = csv.DictReader(file)
+        data = [row for row in reader]
+    with open(OUTPUT_FILENAME, 'w') as file:
+        json.dump(data, file, indent=4)
+
+    ...  # TODO Сериализовать в файл с отступами равными 4
+
+
+if __name__ == '__main__':
+    # Нужно для проверки
+    task()
+
+    with open(OUTPUT_FILENAME) as output_f:
+        for line in output_f:
+            print(line, end="")
